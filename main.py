3ecf644e9ce45d879e747295cfc4b730b1245561
Стас Венгер <stasveng@gmail.com>
2023-01-28 18:14:27 +0200

    version 1.1

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("main file")
+class Student:
+    # Атрибуты уровня класа
+    group = 'B29234'
+    education = 'ITStep'
+    teacher= 'Evganiy'
+
+    def __init__(self):
+        print(id(self))
+
+st1 = Student()
+print(id(st1))
+
+print(Student.group)
+print(Student.teacher)
+print(Student.education)
---
+++ test.py
@@ -0,0 +1,32 @@
+class Student:
+    group = 'B29234'
+    education = 'ITStep'
+    teacher= 'Evganiy'
+    number= '12'
+
+class Stas:
+    name = 'Stas'
+    lastname = 'Venger'
+    top_in_group= '1'
+    age= '12'
+
+class PC:
+    video_card = 'GeForce GTX 1060 6GB'
+    CPU = 'Intel core i5-6600k'
+    RAM= '8 GB'
+    Camera= 'A4tech'
+
+print(Student.group)
+print(Student.teacher)
+print(Student.education)
+print(Student.number)
+
+print(Stas.name)
+print(Stas.lastname)
+print(Stas.top_in_group)
+print(Stas.age)
+
+print(PC.video_card)
+print(PC.CPU)
+print(PC.RAM)
+print(PC.Camera)
